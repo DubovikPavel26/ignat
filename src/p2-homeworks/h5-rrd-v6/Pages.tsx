5c05dbc44010eb1bbbf42d544b433411de626fa5
pavel <dubovikdmc@gmail.com>
2022-11-01 17:38:38 +0300

    1

--- src/p2-homeworks/h5-rrd-v6/Pages.tsx
+++ src/p2-homeworks/h5-rrd-v6/Pages.tsx
@@ -1,26 +1,35 @@
 import React from 'react'
+import {Navigate, Route, Routes} from "react-router-dom";
+import Error404 from "./pages/Error404";
+import PreJunior from "./pages/PreJunior";
+import Junior from "./pages/Junior";
+import JuniorPlus from "./pages/Junior+";
 
 export const PATH = {
     PRE_JUNIOR: '/pre-junior',
+    JUNIOR: '/junior',
+    JUNIORPLUS: '/junior+'
     // add paths
 }
 
 function Pages() {
     return (
         <div>
-            {/*Routes выбирает первый подходящий роут*/}
-            {/*<Routes>*/}
 
-                {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу PRE_JUNIOR*/}
-                {/*<Route path={'/'} element={<Navigate to={PATH.PRE_JUNIOR}/>}/>*/}
+            <Routes>
 
-                {/*<Route path={PATH.PRE_JUNIOR} element={<PreJunior/>}/>*/}
+                в начале мы попадаем на страницу '/' и переходим сразу на страницу PRE_JUNIOR
+                <Route path={'/'} element={<Navigate to={PATH.PRE_JUNIOR}/>}/>
+
+                <Route path={PATH.PRE_JUNIOR} element={<PreJunior/>}/>
+                <Route path={PATH.JUNIOR} element={<Junior/>}/>
+                <Route path={PATH.JUNIORPLUS} element={<JuniorPlus/>}/>
                 // add routes
 
-                {/*он отрисуется если пользователь захочет попасть на несуществующую страницу*/}
-                {/*<Route path={'/*'} element={<Error404/>}/>*/}
+                он отрисуется если пользователь захочет попасть на несуществующую страницу
+                <Route path={'/*'} element={<Error404/>}/>
 
-            {/*</Routes>*/}
+            </Routes>
         </div>
     )
 }
