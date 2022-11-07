f0db184f35e1ed093a2a59ae73000d59fc8b5f32
pavel <dubovikdmc@gmail.com>
2022-11-07 22:52:37 +0300

    1

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,47 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {UserType} from "../HW8";
+
+export const homeWorkReducer = (state: UserType[], action: tsarType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': {
-            // need to fix
-            return state
+            if (action.payload === "up") {
+                return [...state.sort((a, b) => a.name > b.name ? 1 : -1)]
+            } else if (action.payload === "down") {
+                return [...state.sort((a, b) => a.name > b.name ? -1 : 1)]
+            }
+
         }
+            return state
         case 'check': {
-            // need to fix
-            return state
+            return state.filter(el => el.age > action.payload)
         }
-        default: return state
+        default:
+            return state
+    }
+}
+
+type tsarType = SortUpACType | SortDownACType | FilterAgeType
+
+type SortUpACType = ReturnType<typeof SortUpAC>
+
+const SortUpAC = () => {
+    return {
+        type: 'sort',
+        payload: 'up'
+    } as const
+}
+
+type SortDownACType = ReturnType<typeof SortDownAC>
+const SortDownAC = () => {
+    return {
+        type: 'sort',
+        payload: 'down'
+    } as const
+}
+
+type FilterAgeType = ReturnType<typeof FiltrerAge>
+const FiltrerAge = () => {
+    return {
+        type: 'check',
+        payload: 18
     }
 }
